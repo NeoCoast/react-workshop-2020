24a09634311929239bbee75e912ac7aefbfa2c03
Nicolás Tinte <nico.tinte@gmail.com>
2020-09-13 12:40:58 -0300

    Add example test: Post component

--- src/__tests__/components/Post.test.js
+++ src/__tests__/components/Post.test.js
@@ -1,5 +1,38 @@
+import React from 'react';
+import faker from 'faker';
+import { render } from '@testing-library/react';
+import Post from '@Components/Post';
+
 describe('Post', () => {
-  test('example', () => {
-    expect(true).toBe(true);
+  const props = {
+    content: faker.lorem.paragraph(),
+    title: faker.random.words(),
+    username: faker.internet.userName(),
+  };
+
+  test('shows the title', () => {
+    const { getByText } = render(<Post {...props} />);
+
+    expect(getByText(props.title)).toBeInTheDocument();
+  });
+
+  test('shows the username', () => {
+    const { getByText } = render(<Post {...props} />);
+
+    expect(getByText(props.username)).toBeInTheDocument();
+  });
+
+  test('shows the content', () => {
+    const { getByText } = render(<Post {...props} />);
+
+    expect(getByText(props.content)).toBeInTheDocument();
+  });
+
+  test('show the correct avatar', () => {
+    const { getByRole } = render(<Post {...props} />);
+
+    const avatar = getByRole(/img/i);
+    expect(avatar).toBeInTheDocument();
+    expect(avatar).toHaveAttribute('src', `https://robohash.org/${props.username}`);
   });
 });
